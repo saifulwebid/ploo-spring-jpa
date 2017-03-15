7732f80e77378b83342e95e85a692b7205350ced
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-03-16 06:41:16 +0700

    Initial commit: project created from http://start.spring.io/ Dependencies: Web, JPA, PostgreSQL

---
+++ src/main/java/jtk/ploo/SpringJpaTaskApplication.java
@@ -0,0 +1,12 @@
+package jtk.ploo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaTaskApplication.class, args);
+	}
+}
