0484ebc358d0a0a7f9a19f45f4b46fc71c27166d
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-04-20 07:44:30 +0700

    Change FacultyController.findById() to FacultyController.delete() Similar signature; purpose changed.

--- src/main/java/jtk/ploo/controller/FacultyController.java
+++ src/main/java/jtk/ploo/controller/FacultyController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -49,9 +50,11 @@ public class FacultyController {
 		return "redirect:/faculty/list";
 	}
 	
-	@RequestMapping("/findById")
-	public Faculty findById(@RequestParam("id") Long id) {
-		return repository.findOne(id);
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String delete(@PathVariable("id") Long id) {
+		repository.delete(repository.findOne(id));
+		
+		return "redirect:/faculty/list";
 	}
 	
 }
